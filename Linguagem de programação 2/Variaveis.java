c669db01543f2c75534d5fdfaf23d66004949f85
Lisabueno <lisa_tkm@live.com>
2021-09-07 18:26:39 -0300

    Curso TI 2 semestre

---
+++ Linguagem de programação 2/Variaveis.java
@@ -0,0 +1,10 @@
+public class Variaveis{
+    public static void main(String[] args) {
+        int i1 = 0, i2;
+        double d1, d2;
+        String s1, s2;
+        boolean b1, b2;
+
+        System.out.println("i1: " + i1);
+    }
+}
